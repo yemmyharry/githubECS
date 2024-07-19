1143a1720220b64d3ef0f66fa3422235db98858d
yemmyharry <yemmyharry@gmail.com>
2024-07-19 07:53:28 +0100

    feat: entry point

---
+++ cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"githubECS/internal/routes"
+	"githubECS/internal/scheduler"
+	"githubECS/pkg/db"
+	"log"
+	"os"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	database, err := db.Initialize(dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	scheduler.StartScheduler(database)
+
+	r := gin.Default()
+	routes.SetupRouter(r, database)
+
+	r.Run()
+}
