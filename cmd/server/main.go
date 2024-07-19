78e42ccf5cc7db1214a88996a7dafecbf9020177
yemmyharry <yemmyharry@gmail.com>
2024-07-19 14:49:14 +0100

    refactor: clean up

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,38 +1,3 @@
-// package main
-//
-// import (
-//
-//	"github.com/gin-gonic/gin"
-//	"github.com/joho/godotenv"
-//	"githubECS/internal/routes"
-//	"githubECS/internal/scheduler"
-//	"githubECS/pkg/db"
-//	"log"
-//	"os"
-//
-// )
-//
-//	func main() {
-//		if err := godotenv.Load(); err != nil {
-//			log.Fatal("Error loading .env file")
-//		}
-//
-//		dsn := os.Getenv("DATABASE_URL")
-//		database, err := db.Initialize(dsn)
-//		if err != nil {
-//			log.Fatalln(err)
-//		}
-//
-//		scheduler.StartScheduler(database)
-//
-//		r := gin.Default()
-//		routes.SetupRouter(database)
-//
-//		err = r.Run(":8089")
-//		if err != nil {
-//			return
-//		}
-//	}
 package main
 
 import (
@@ -45,7 +10,6 @@ import (
 )
 
 func main() {
-	// Load environment variables from .env file if it exists
 	_ = godotenv.Load()
 
 	dsn := os.Getenv("DATABASE_URL")
